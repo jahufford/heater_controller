40ca561f606f14c9d258408ac9c4970088d7891d
Joe Hufford <jahufford@gmail.com>
2018-01-03 18:37:10 -0700

    fixing eclipse bullshit

--- src/main.cpp
+++ src/main.cpp
@@ -442,15 +442,15 @@ int main(void)
 //    if(xTaskCreate(GUITask, "GuiTask", 512,0,0,0) != pdPASS){ // very low priority
 //
 //    }
-    auto gui_func = [](void *disp){
-    	auto display = static_cast<Display*>(disp);
-    	display->RunDisplay();
-    };
-    // wire up gui task
-    if(xTaskCreate(gui_func,"Gui Task", 512,static_cast<void*>(&display),0,0) != pdPASS)
-    {
-    	// TODO handle error
-    }
+//    auto gui_func = [](void *disp){
+//    	auto display = static_cast<Display*>(disp);
+//    	display->RunDisplay();
+//    };
+//    // wire up gui task
+//    if(xTaskCreate(gui_func,"Gui Task", 512,static_cast<void*>(&display),0,0) != pdPASS)
+//    {
+//    	// TODO handle error
+//    }
 //
 //    //queue2 = xQueueCreate(1,1);
 //    xTaskCreate(vTask1,"Task 1", 500,NULL,1,NULL);
